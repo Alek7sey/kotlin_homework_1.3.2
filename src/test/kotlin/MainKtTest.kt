eca25a1a3de2a134f651848e8f8e3a8eb88f6acd
Alexey Ashikhmin <ash60782@gmail.com>
2023-04-02 19:22:49 +0500

    Error test mainKtTest

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -28,9 +28,9 @@ class MainKtTest {
         val result3 = calcComission("Mir", 50_000, 10_000)
         assertEquals(75, result3)
         val result4 = calcComission("VK Pay", 40_000, 30_000)
-        assertEquals(-2, result4)
+        assertEquals(-1, result4)
         val result5 = calcComission("Mastercard", 10_000, 200_000)
-        assertEquals(-1, result5)
+        assertEquals(-2, result5)
         val result6 = calcComission("Visa", 35_000, 20_000)
         assertEquals(150, result6)
 
